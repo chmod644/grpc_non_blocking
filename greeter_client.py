99bc0ab742633bf11fa71a7457b460a6afe84ca7
someone <someone@example.com>
2017-01-22 15:37:06 +0900

    Try cancel

--- greeter_client.py
+++ greeter_client.py
@@ -5,6 +5,7 @@
 
 from __future__ import print_function
 
+import time
 import grpc
 
 import helloworld_pb2
@@ -15,19 +16,27 @@ def send():
     stub = helloworld_pb2.GreeterStub(channel)
     request = helloworld_pb2.HelloRequest(name='you')
     future = stub.SayHello.future(request)
+    print("Greeter client send request")
     return future
 
 
 def recieve(future):
     response = future.result()
+    print("Greeter client received response: " + response.message)
     return response
 
 
+def cancel(future):
+    ret = future.cancel()
+    print("Greeter client send cancel")
+
+
 def run():
-    future = send()
-    print("Greeter client send request")
-    response = recieve(future)
-    print("Greeter client received response: " + response.message)
+    future1 = send()
+    future2 = send()
+    time.sleep(1)
+    cancel(future1)
+    response = recieve(future2)
 
 
 if __name__ == '__main__':
--- greeter_server.py
+++ greeter_server.py
@@ -6,6 +6,7 @@
 from concurrent import futures
 import time
 
+import time
 import grpc
 
 import helloworld_pb2
@@ -15,23 +16,28 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 class Greeter(helloworld_pb2.GreeterServicer):
 
-  def SayHello(self, request, context):
-    return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
+    def SayHello(self, request, context):
+        print("Greeter server received request")
+        time.sleep(10)
+        print("Greeter server send response")
+        return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
 
-  def SayHelloAgain(self, request, context):
-    return helloworld_pb2.HelloReply(message='Hello again, %s!' % request.name)
+    def SayHelloAgain(self, request, context):
+        print("Greeter server received request")
+        print("Greeter server send response")
+        return helloworld_pb2.HelloReply(message='Hello again, %s!' % request.name)
 
 
 def serve():
-  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  helloworld_pb2.add_GreeterServicer_to_server(Greeter(), server)
-  server.add_insecure_port('[::]:50051')
-  server.start()
-  try:
-    while True:
-      time.sleep(_ONE_DAY_IN_SECONDS)
-  except KeyboardInterrupt:
-    server.stop(0)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
 
 if __name__ == '__main__':
-  serve()
+    serve()
